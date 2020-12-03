7177efabcc355e97ed1d16cc9aad1fb14c8dc2ad
caioribeiroo <caioxj3@gmail.com>
2020-12-02 21:31:51 -0300

    adicionando patterns melhorias

--- src/patterns/InvoiceFacade.java
+++ src/patterns/InvoiceFacade.java
@@ -1,12 +1,17 @@
 package patterns;
+import java.util.Date;
 
 public class InvoiceFacade {
+    Clock clock = new SystemClock();
     protected InvoiceFacade(){}
     
     public String getClient(String client){
         return client;
     }
     public String createBill(String client, double value){
+        String foundClient = getClient(client);
+        Date currentDay = clock.current().getTime();
+        return "a fatura no valor de" + value + ", foi gerada para no seu cpf " + foundClient +  " no dia:" + currentDay +".";   
     }
     public void dispatchContact(String client, String bill){
         String foundClient = getClient(client);
